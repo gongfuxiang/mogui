1d47b571e7c7f6bd9dd7ddb506414fc389420874
gongfuxiang <fuxiang.gong@qq.com>
2017-08-17 23:00:07 +0800

    时区设置

--- mogui/settings.py
+++ mogui/settings.py
@@ -123,13 +123,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
