733b652aafdae1323f2a5c9a3ebfaeba5eb5e211
gongfuxiang <fuxiang.gong@qq.com>
2017-08-19 00:07:09 +0800

    vue使用本地源码

--- mogui/settings.py
+++ mogui/settings.py
@@ -136,6 +136,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/public/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "public"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, "public")
 ]
